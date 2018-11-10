b63e8a17e8ca86f084714d04d4ab610feb1dc0cd
Masaki Hara <ackie.h.gmai@gmail.com>
2018-11-10 19:28:22 +0900

    Add Mutex.

--- src/unsync.rs
+++ src/unsync.rs
@@ -1,1 +1,15 @@
+use std::cell::{Cell, UnsafeCell};
 
+pub struct Mutex<T: ?Sized> {
+    locked: Cell<bool>,
+    data: UnsafeCell<T>,
+}
+
+impl<T> Mutex<T> {
+    pub fn new(inner: T) -> Self {
+        Self {
+            locked: Cell::new(false),
+            data: UnsafeCell::new(inner),
+        }
+    }
+}
