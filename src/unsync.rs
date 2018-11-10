cb14ca05a38948b33e66ae56765bf043fe27b3a7
Masaki Hara <ackie.h.gmai@gmail.com>
2018-11-10 19:30:37 +0900

    Implement into_inner and get_mut.

--- src/unsync.rs
+++ src/unsync.rs
@@ -12,4 +12,17 @@ impl<T> Mutex<T> {
             data: UnsafeCell::new(inner),
         }
     }
+
+    pub fn into_inner(self) -> T {
+        let Self { data, .. } = self;
+        let inner = data.into_inner();
+        inner
+    }
+}
+
+impl<T: ?Sized> Mutex<T> {
+    pub fn get_mut(&mut self) -> &mut T {
+        let inner = unsafe { &mut *self.data.get() };
+        inner
+    }
 }
